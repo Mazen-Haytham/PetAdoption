bed5551340265c62f5df5eda7f9ecf1f5b14fcd0
omarElsherief <omarelsherief129@gmail.com>
2026-05-09 17:46:24 +0300

    userInfo 'me' endpoint

--- backend/Services/IAuthService.cs
+++ backend/Services/IAuthService.cs
@@ -8,6 +8,7 @@ namespace backend.Services
         Task<bool> RegisterAsync(RegisterRequest request);
         Task<LoginResponse> LoginAsync(LoginRequest request);
         Task<TokenResponseDto?> RefreshTokensAsync(RefreshTokenRequestDto request);
+        Task<UserInfoResponse?> GetUserInfoAsync(int userId);
         Task<bool> LogoutAsync(int userId);
         //Task<bool> UpdateUserStatusAsync(int userId, string decision);
     }
